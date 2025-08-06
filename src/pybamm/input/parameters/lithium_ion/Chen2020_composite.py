51bd67f69edf6b5136284a12074037da66cdc7d6
lorenzplank <lorenz@sphere-energy.eu>
2025-08-06 14:56:23 +0200

    Negative reaction LAM facor

--- src/pybamm/input/parameters/lithium_ion/Chen2020_composite.py
+++ src/pybamm/input/parameters/lithium_ion/Chen2020_composite.py
@@ -785,6 +785,7 @@ def get_parameter_values():
         "Secondary: SEI kinetic rate constant [m.s-1]": 1e-12,
         "Secondary: SEI growth activation energy [J.mol-1]": 0.0,
         "Positive electrode reaction-driven LAM factor [m3.mol-1]": 0.0,
+        "Negative electrode reaction-driven LAM factor [m3.mol-1]": 0.0,
         # cell
         "Negative current collector thickness [m]": 1.2e-05,
         "Negative electrode thickness [m]": 8.52e-05,
