465ee7460cb299dcf04a34558c91bafc4e84b0dc
lorenzplank <lorenz@sphere-energy.eu>
2025-08-11 00:26:05 +0200

    plating

--- src/pybamm/models/submodels/interface/lithium_plating/plating.py
+++ src/pybamm/models/submodels/interface/lithium_plating/plating.py
@@ -94,11 +94,9 @@ class Plating(BasePlating):
         elif lithium_plating_option == "irreversible":
             # j_stripping is always negative, because there is no stripping, only
             # plating
-            j_stripping = pybamm.if_then_else(
-                                                eta_plating <= 0,
-                                                -j0_plating * pybamm.exp(F_RT * alpha_plating * eta_plating),
-                                                pybamm.Scalar(0)
-                                            )
+            j_stripping = pybamm.Minimum(0, eta_plating)/eta_plating * (
+                -j0_plating * pybamm.exp(F_RT * alpha_plating * eta_plating)
+            )
 
         variables.update(self._get_standard_overpotential_variables(eta_stripping))
         variables.update(self._get_standard_reaction_variables(j_stripping))
