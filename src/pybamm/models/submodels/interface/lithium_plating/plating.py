0af019b26859c948d083675e2887fd3aa10ee822
lorenzplank <lorenz@sphere-energy.eu>
2025-08-11 13:51:41 +0200

    plating

--- src/pybamm/models/submodels/interface/lithium_plating/plating.py
+++ src/pybamm/models/submodels/interface/lithium_plating/plating.py
@@ -92,7 +92,7 @@ class Plating(BasePlating):
             ) - j0_plating * pybamm.exp(F_RT * alpha_plating * eta_plating)
         elif lithium_plating_option == "irreversible":
             # j_stripping is zero when eta_plating > 0, otherwise follows exponential decay
-            j_stripping = 0.5 * (1 + pybamm.tanh(100 * eta_plating)) * (
+            j_stripping = 0.5 * (1 + pybamm.tanh(1000 * eta_plating)) * (
                 -j0_plating * pybamm.exp(F_RT * alpha_plating * eta_plating)
             )
 
